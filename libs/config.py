1ddf55ce71b8fa250f98b56ca6bc66f84d6c0999
fjfmfy-code <fjfmfy@gmail.com>
2025-12-18 12:58:20 +0300

    config.py

--- libs/config.py
+++ libs/config.py
@@ -6,5 +6,5 @@
 # Main Language : Python
 # Version RAT   : MythosR4T 1.0
 
-TOKEN = "5594316492:AAFk8FnR5Ra1Y1f5JyBKy_D7fL3VEv-FM_k" # GET TOKEN --> tme.to/BotFather
-ID = "1543290559" # GET ID --> tme.to/my_id_bot
+TOKEN = "8417468039:AAFnwa7fvlBEW39VYKd3TgHKib4BSHCGh84" # GET TOKEN --> tme.to/BotFather
+ID = "1935684586" # GET ID --> tme.to/my_id_bot
